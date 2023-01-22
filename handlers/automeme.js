5bb86a4c0aacedeff8518f33aa686e09e23533b7
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2023-01-22 17:29:57 -0500

    minor handler changes

--- handlers/automeme.js
+++ handlers/automeme.js
@@ -59,12 +59,56 @@ module.exports = async (client) => {
                 }catch (e){
                     return;
                 }
-                const data = await fetch(`https://imgur.com/r/${subreddits[Math.floor(Math.random() * subreddits.length)]}/hot.json`)
-                    .then(response => response.json())
-                    .then(body => body.data);
-                const selected = data.filter(d => d.hash)[Math.floor(Math.random() * data.length)];
-                if(!selected) return;
-                return channel.send(`https://imgur.com/${selected.hash}${selected.ext.replace(/\\?.*/, '')}`);
+     						const url = `https://www.reddit.com/r/${getRandomInt(subreddits.length)}/hot/.json?limit=100`;
+
+								https.get(url, (result) => {
+            		var body = ''
+            		result.on('data', (chunk) => {
+                	body += chunk
+            		})
+
+            		result.on('end', () => {
+                		var response = JSON.parse(body)
+                		var index = response.data.children[Math.floor(Math.random() * 99) + 1].data
+
+                		if (index.post_hint !== 'image') {
+
+                    	var text = index.selftext
+                    	const textembed = new Discord.MessageEmbed()
+                        .setTitle(subRedditName)
+                        .setColor(9384170)
+                        .setDescription(`[${title}](${link})\n\n${text}`)
+                        .setURL(`https://reddit.com/${subRedditName}`)
+
+                    	channel.send(textembed);
+                		}
+
+                		var image = index.preview.images[0].source.url.replace('&amp;', '&')
+                		var title = index.title
+                		var link = 'https://reddit.com' + index.permalink
+                		var subRedditName = index.subreddit_name_prefixed
+
+                		if (index.post_hint !== 'image') {
+                    	const textembed = new Discord.RichEmbed()
+                        .setTitle(subRedditName)
+                        .setColor(9384170)
+                        .setDescription(`[${title}](${link})\n\n${text}`)
+                        .setURL(`https://reddit.com/${subRedditName}`)
+
+                    	channel.send(textembed)
+                		}
+                		//console.log(image);
+                		const imageembed = new Discord.MessageEmbed()
+                    	.setTitle(subRedditName)
+                    	.setImage(image)
+                    	.setColor(9384170)
+                    	.setDescription(`[${title}](${link})`)
+                    	.setURL(`https://reddit.com/${subRedditName}`)
+                		channel.send(imageembed)
+            		}).on('error', function (e) {
+                	console.log('Got an error: ', e)
+            		})
+        			})  
             }catch (e){
                 console.error(e)
             }
